1500ada670c0ea3c36d493c055ca84e0386555e6
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-09-25 10:45:39 +0530

    create login-signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -10,6 +12,8 @@ const App = () => {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </Router>
