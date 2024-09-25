e9bc433e79c9ca0f422a9916328498d86f48a985
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-09-25 09:26:47 +0530

    create token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,10 @@
 const User = require("./../models/userModel");
 const errorHandler = require("../utils/errorHandler");
+const jwt = require("jsonwebtoken");
 
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 // login user
 exports.loginUser = async (req, res) => {
   res.json({ msg: "user logged in" });
@@ -8,12 +12,12 @@ exports.loginUser = async (req, res) => {
 
 //signup user
 exports.signupUser = async (req, res, next) => {
-    const {email,password} = req.body;
-    try{
-        const user = await User.signup(email,password);
-        res.status(200).json({email,user});
-
-    }catch(error){
-        next(errorHandler(400,error.message));
-    }
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    next(errorHandler(400, error.message));
+  }
 };
