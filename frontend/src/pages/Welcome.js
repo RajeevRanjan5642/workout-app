814abf25fe18e945443d3947b871787a0103b463
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-09-29 12:24:34 +0530

    add responsiveness

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -4,14 +4,19 @@ const Welcome = () => {
   const navigate = useNavigate();
   return (
     <div className="welcome">
-      <img src={process.env.PUBLIC_URL + "/yog.jpeg"} alt="" className="welcome-img"/>
+      <img
+        src={process.env.PUBLIC_URL + "/yog.jpeg"}
+        alt=""
+        className="welcome-img"
+      />
       <div className="welcome-content">
-        <h1 className="welcome-title">Welcome to Workout Buddy</h1>
+        <h1 className="welcome-title">Your Daily Workout Planner</h1>
+
         <p className="welcome-text">
           Keep a detailed record of your exercises and track your progress with
-          Workout Buddy, your ultimate exercise log. Whether you’re lifting weights,
-          running, or practicing yoga, our web app helps you stay organized and
-          motivated.
+          Workout Buddy, your ultimate exercise log. Whether you’re lifting
+          weights, running, or practicing yoga, our web app helps you stay
+          organized and motivated.
         </p>
         <button className="welcome-btn" onClick={() => navigate("/login")}>
           Get Started
