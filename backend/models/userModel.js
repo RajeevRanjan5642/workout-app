e6266201cec9a645de8c15780b85cbe085f45dd9
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-10-02 11:29:08 +0530

    update user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,7 +35,7 @@ userSchema.statics.signup = async function (email, password, isVerified) {
 
   const exists = await this.findOne({ email });
 
-  if (exists.isVerified) {
+  if (exists && exists.isVerified) {
     throw Error("User already exists");
   }
 
