0e69631cca7a1f72fb5702d69e8447bf6cfc0cd7
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-10-02 11:26:47 +0530

    update user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,8 +8,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: true
   },
   password: {
     type: String,
