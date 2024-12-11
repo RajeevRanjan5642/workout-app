eb435c1ed739879ab1623629d7e172f6cc97af8c
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-12-11 20:36:29 +0530

    page-not-found

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <div className="not-found">
-      <img src={process.env.PUBLIC_URL + "/not_found.jpg"} alt="" className="not-found-img"/>
+      <img src={process.env.PUBLIC_URL + "/404.jpg"} alt="" className="not-found-img"/>
       <Link to="/" className="back-to-home">Back to Home</Link>
     </div>
   );
